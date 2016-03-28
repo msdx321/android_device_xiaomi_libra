ca9bc498ae8982aa6512f5ed63c8daf8926ee09f
crancocco <shulaibai@hustunique.com>
2016-03-28 18:02:09 +0800

    libra:add CAMERA_VENDOR_L_COMPAT flag
    Change-Id: I3fc2664b18a83f0c9ee3b70511b9fe6bdacc23c3

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -134,6 +134,7 @@ USE_DEVICE_SPECIFIC_CAMERA := true
 # Once camera module can run in the native mode of the system (either
 # 32-bit or 64-bit), the following line should be deleted
 BOARD_QTI_CAMERA_32BIT_ONLY := true
+COMMON_GLOBAL_CFLAGS += -DCAMERA_VENDOR_L_COMPAT
 
 #Light HAL
 TARGET_PROVIDES_LIBLIGHT := true
