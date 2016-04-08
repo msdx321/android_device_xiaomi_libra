c526469c492ff20b452453db65f2ec29861a24b8
crancocco <shulaibai@hustunique.com>
2016-04-08 22:51:46 +0800

    libra:add VibratorHW

---
+++ mkhw/org/mokee/hardware/VibratorHW.java
@@ -0,0 +1,54 @@
+/*
+ * Copyright (C) 2016 The CyanogenMod Project
+ * Copyright (C) 2016 The Mokee OpenSource Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.mokee.hardware;
+
+import org.mokee.hardware.util.FileUtils;
+import java.io.File;
+
+public class VibratorHW {
+
+    private static String LEVEL_PATH = "/sys/devices/virtual/timed_output/vibrator/amp";
+
+    public static boolean isSupported() {
+        return new File(LEVEL_PATH).exists();
+    }
+
+    public static int getMaxIntensity()  {
+        return 100;
+    }
+
+    public static int getMinIntensity()  {
+        return 50;
+    }
+
+    public static int getWarningThreshold()  {
+        return 90;
+    }
+
+    public static int getCurIntensity()  {
+        return Integer.parseInt(FileUtils.readOneLine(LEVEL_PATH));
+    }
+
+    public static int getDefaultIntensity()  {
+        return 79;
+    }
+
+    public static boolean setIntensity(int intensity)  {
+        return FileUtils.writeLine(LEVEL_PATH, String.valueOf(intensity));
+    }
+}
