52e26f4df2d48a9aa348d12fdceb61cb342f0c28
crancocco <shulaibai@hustunique.com>
2016-04-14 15:37:22 +0800

    update camera shim lib

--- camera/Android.mk
+++ camera/Android.mk
@@ -11,14 +11,17 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+
 LOCAL_PATH := $(call my-dir)
+
 include $(CLEAR_VARS)
 
-LOCAL_SRC_FILES := \
-    libcamera_shim.c
+LOCAL_SRC_FILES := camera/sensor.cpp camera/SensorManager.cpp camera/Threads.c
 
-LOCAL_SHARED_LIBRARIES := libutils libgui liblog libcutils libbinder libui
+LOCAL_C_INCLUDES := camera
+LOCAL_SHARED_LIBRARIES := libgui libutils liblog libbinder libandroid
 LOCAL_MODULE := libcamera_shim
 LOCAL_MODULE_TAGS := optional
+LOCAL_MODULE_CLASS := SHARED_LIBRARIES
 
 include $(BUILD_SHARED_LIBRARY)
