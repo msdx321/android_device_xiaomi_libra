08a9112e7bfd760a5fe6850724192a7f5de386ee
crancocco <shulaibai@hustunique.com>
2016-04-18 17:48:46 +0800

    add camera wrapper

--- camera/Android.mk
+++ camera/Android.mk
@@ -25,3 +25,21 @@ LOCAL_MODULE_TAGS := optional
 LOCAL_MODULE_CLASS := SHARED_LIBRARIES
 
 include $(BUILD_SHARED_LIBRARY)
+
+include $(CLEAR_VARS)
+
+LOCAL_SRC_FILES := \
+    CameraWrapper.cpp
+
+LOCAL_SHARED_LIBRARIES := \
+    libhardware liblog libutils libgui
+
+LOCAL_C_INCLUDES += \
+    system/media/camera/include
+
+LOCAL_MODULE_RELATIVE_PATH := hw
+LOCAL_MODULE := camera.$(TARGET_BOARD_PLATFORM)
+LOCAL_MODULE_TAGS := optional
+
+LOCAL_32_BIT_ONLY := $(BOARD_QTI_CAMERA_32BIT_ONLY)
+include $(BUILD_SHARED_LIBRARY)
